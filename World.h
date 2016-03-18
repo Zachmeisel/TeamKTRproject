1f31ffa230d4beac18a161a0c5bbac4df9876224
C-Timm <christopher.timm@student.dwc.edu>
2016-03-17 20:34:59 -0400

    Update World.h
    fixed errors

--- World.h
+++ World.h
@@ -3,30 +3,27 @@
 #include "Robot.h"
 #include "Beeper.h"
 
+using namespace System;
+
 ref class World
 {
 private:
 	//Beeper^ myBeeper;
 	Robot^ myRobot;
 	array <Cell^, 2>^ grid;
+	
+
 public:
-	static const int NUMROWS = 8;
-	static const int NUMCOLS = 8;
+	int wHeight, wWidth;
 	World()
 	{
-		//Create cheese
-		//myBeeper = gcnew Beeper();
-
-		//Create mouse
-		//myRobot = gcnew Robot();
-
 		//Construct grid array
-		grid = gcnew array<Cell^, 2>(NUMROWS, NUMCOLS);
-		for (int row = 0; row < NUMROWS; row++)
+		grid = gcnew array<Cell^, 2>(wHeight, wWidth);
+		for (int row = 0; row < wHeight; row++)
 		{
-			for (int col = 0; col < NUMCOLS; col++)
+			for (int col = 0; col < wWidth; col++)
 			{
-				grid[row, col] = gcnew Cell(row, col, true);
+				grid[row, col] = gcnew Cell(row, col);
 			}
 		}
 	};
